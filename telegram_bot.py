f9ca21d671bd4ed7cc8cb6f168a43e225356e5f5
junho <kesu9251@naver.com>
2021-07-16 18:38:42 +0900

    init

---
+++ telegram_bot.py
@@ -0,0 +1,35 @@
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+class TelegramBot:
+
+    def __init__(self):
+        self.core = None
+        self.updater = None
+        self.id = None
+
+    def SetChatId(self, chat_id):
+        self.id = chat_id
+
+    def SetToken(self, token):
+        self.core = telegram.Bot(token)
+        self.updater = Updater(token)
+
+    def SendMsg(self, text):
+        self.core.sendMessage(chat_id=self.id, text=text)
+
+
+if __name__ == '__main__':
+    with open('private.txt', 'r') as f:
+        data = f.read()
+        data = data.split('\n')
+        for i in data:
+            if 'telegramtoken' in i:
+                token = i[i.find(':') + 1:]
+            elif 'telegramchatid' in i:
+                chatid = i[i.find(':') + 1:]
+    tgBot = TelegramBot()
+    tgBot.SetChatId(chatid)
+    tgBot.SetToken(token)
+    tgBot.SendMsg('a')
+    #tlgBot.SendMsg('hello_world')
