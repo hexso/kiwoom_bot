b296193861e05516538af65985b3c9fab813b65d
junho <kesu9251@naver.com>
2022-02-20 20:19:43 +0900

    시장가, 지정가 구매 및 주식가격 밭아오기 구현
    Signed-off-by: junho <kesu9251@naver.com>

--- kiwoom_api.py
+++ kiwoom_api.py
@@ -9,7 +9,8 @@ class KiwoomApi:
 
     def __init__(self):
         self.kiwoom = Kiwoom()
-
+        self.scrno = '1234'
+        self.request_no = 0
         if self.kiwoom.kiwoom_GetConnectState() is not 0:
             print('연결되지 않았음')
             return False
@@ -42,3 +43,20 @@ class KiwoomApi:
             time.sleep(SLEEP_TIME)
         balance = self.kiwoom.int_주문가능금액
         return balance
+
+    def limit_order(self, stockcode, price, amount):
+        self.request_no += 1
+        sRQName = 'RQ_'+ str(self.request_no)
+        result = self.kiwoom.kiwoom_SendOrder(sRQName, self.scrno, self.account, 1, str(stockcode), amount, price, '00','')
+        return result
+
+    def market_order(self, stockcode, amount):
+        self.request_no += 1
+        sRQName = 'RQ_' + str(self.request_no)
+        result = self.kiwoom.kiwoom_SendOrder(sRQName, self.scrno, self.account, 1, str(stockcode), amount, 0, '01', '')
+        return result
+
+    def get_now_stock_price(self, stockcode):
+        stockcode = str(stockcode)
+        result = self.kiwoom.kiwoom_TR_OPT10080_주식분봉차트조회(stockcode, fix=0)
+        return result['result'][0]
